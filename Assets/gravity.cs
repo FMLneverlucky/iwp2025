82b9ab269d91aed1c87c20eed5f1da22d352f24c
FMLneverlucky <jingjie0506@gmail.com>
2025-07-03 10:23:18 +0800

    add working constraint for bottom of playing field bug: cannot do grab n release consecutively without restarting game

--- Assets/gravity.cs
+++ Assets/gravity.cs
@@ -1,28 +1,52 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class gravity : MonoBehaviour
 {
     public InputActionAsset InputAction;
     private InputAction grab;
-    private float veloctiy;
+    private InputAction release;
     private Rigidbody2D rb;
-    //private InputAction release;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField]
+    private Tilemap playingField;
+    private BoxCollider2D objectCollider;
     void Start()
     {
         grab = InputSystem.actions.FindAction("Interact");
+        release = InputSystem.actions.FindAction("Interact2");
         rb = GetComponent<Rigidbody2D>();
+        objectCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (grab.WasPressedThisFrame())
+        //enable gravity
+        if (grab.WasPressedThisFrame() && !rb.simulated)
         {
+            rb.gravityScale = 1;
             rb.simulated = true; //gravity affects objects when "simulated" is active
         }
-        //enable gravity
-        //disable gravity
+        else if (release.WasPressedThisFrame() && !rb.simulated)
+        {
+            rb.gravityScale = -1; //this gravity scale does make object move upwards, just having issues making this work after grabbing action
+            rb.simulated = true;
+        }
+        Constrained();
+    }
+
+    private bool Constrained()
+    {
+        BoxCollider2D fieldCollider = playingField.GetComponent<BoxCollider2D>();
+        float fieldboundY = fieldCollider.bounds.min.y;
+        float objectboundY = objectCollider.bounds.min.y;
+        if (objectboundY < fieldboundY)
+        {
+            rb.gravityScale = 0;
+            rb.simulated = false;
+        }
+        return rb.simulated;
     }
 }
