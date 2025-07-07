ecdadecfe5237f190d3ad799a02dd4427a8ba926
FMLneverlucky <jingjie0506@gmail.com>
2025-07-07 12:56:48 +0800

    added top constraint of playing field technically works, but bubbles will run off in previous direction before changing directions for some reason

--- Assets/gravity.cs
+++ Assets/gravity.cs
@@ -8,6 +8,13 @@ public class gravity : MonoBehaviour
     private InputAction grab;
     private InputAction release;
     private Rigidbody2D rb;
+    private enum gravityState
+    {
+        None = 0,
+        antiGravity,
+        gravity
+    }
+    private gravityState currentState = gravityState.None;
 
     [SerializeField]
     private Tilemap playingField;
@@ -27,24 +34,52 @@ public class gravity : MonoBehaviour
         if (grab.WasPressedThisFrame() && !rb.simulated)
         {
             rb.gravityScale = 1;
+            currentState = gravityState.gravity;
             rb.simulated = true; //gravity affects objects when "simulated" is active
         }
         else if (release.WasPressedThisFrame() && !rb.simulated)
         {
             rb.gravityScale = -1; //this gravity scale does make object move upwards, just having issues making this work after grabbing action
+            currentState = gravityState.antiGravity;
             rb.simulated = true;
         }
-        Constrained();
+        Constrained(currentState);
     }
 
-    private bool Constrained()
+    private bool Constrained(gravityState state)
     {
+        bool detected = false;
         BoxCollider2D fieldCollider = playingField.GetComponent<BoxCollider2D>();
-        float fieldboundY = fieldCollider.bounds.min.y;
-        float objectboundY = objectCollider.bounds.min.y;
-        if (objectboundY < fieldboundY)
+        float fieldBoundsY = 0; //temp value, will be assigned in switch case below
+        float objectBoundsY = 0;
+
+        switch (state)
+        {
+            case gravityState.None:
+                break;
+            case gravityState.antiGravity:
+                {
+                    //check top of boxes
+                    fieldBoundsY = fieldCollider.bounds.max.y;
+                    objectBoundsY = objectCollider.bounds.max.y;
+                    if (objectBoundsY > fieldBoundsY) //top of bubble collider is above playing field
+                        detected = true;
+                    break;
+                }
+            case gravityState.gravity:
+                {
+                    //check bottom of boxes
+                    fieldBoundsY = fieldCollider.bounds.min.y;
+                    objectBoundsY = objectCollider.bounds.min.y;
+                    if (objectBoundsY < fieldBoundsY) //bottom of bubble collide is below playing field
+                        detected = true;
+                    break;
+                }
+        }
+        if (detected)
         {
             rb.gravityScale = 0;
+            currentState = gravityState.None;
             rb.simulated = false;
         }
         return rb.simulated;
