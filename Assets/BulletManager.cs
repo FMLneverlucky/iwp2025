837e2bd2c379cdfab0d23466441197a50d3a89aa
FMLneverlucky <jingjie0506@gmail.com>
2025-07-30 11:56:01 +0800

    constraint bullet to play area (bullet deletes when going out of background boundary)

--- Assets/BulletManager.cs
+++ Assets/BulletManager.cs
@@ -10,6 +10,8 @@ public class BulletManager : MonoBehaviour
 {
     private static BulletManager bManagerInstance;
     private List<GameObject> bulletList;
+    private static float cd = 0.5f;
+    private static bool shot = false;
 
     BulletManager()
     {
@@ -28,14 +30,18 @@ public class BulletManager : MonoBehaviour
 
     public void CreateBullet()
     {
-        GameObject bullet = GameObject.Find("bullet");
-        GameObject player = GameObject.Find("player");
-        GameObject bulletGroup = GameObject.Find("player bullets");
+        if (!shot)
+        {
+            GameObject bullet = GameObject.Find("bullet");
+            GameObject player = GameObject.Find("player");
+            GameObject bulletGroup = GameObject.Find("player bullets");
 
-        GameObject bullet_instance = Instantiate(bullet, player.transform.position, Quaternion.identity, bulletGroup.transform);   //object to instantiate, position to instantiate, rotation, parent object
-        bullet_instance.GetComponent<SpriteRenderer>().enabled = true;
+            GameObject bullet_instance = Instantiate(bullet, player.transform.position, Quaternion.identity, bulletGroup.transform);   //object to instantiate, position to instantiate, rotation, parent object
+            bullet_instance.GetComponent<SpriteRenderer>().enabled = true;
 
-        bulletList.Add(bullet_instance); //add to list for frame update
+            bulletList.Add(bullet_instance); //add to list for frame update
+            shot = true;
+        }
     }
 
     public void Update()
@@ -46,7 +52,34 @@ public class BulletManager : MonoBehaviour
             {
                 bulletList[c].GetComponent<Rigidbody2D>().linearVelocity = Vector3.up * 3;
                 bulletList[c].GetComponent<Rigidbody2D>().simulated = true;
+                if (OutofBounds(bulletList[c]))
+                {
+                   bulletList[c].SetActive(false);
+                   bulletList.Remove(bulletList[c]);
+                   break;
+                }
             }
         }
+        if ((shot) && (cd > 0))
+        {
+            cd -= Time.deltaTime;
+        }
+        else if (cd <= 0)
+        {
+            cd = 0.5f;
+            shot = false;
+        }
+    }
+
+    //checks if bullet is outside playarea
+    private bool OutofBounds(GameObject obj)
+    {
+        Bounds playarea_bounds = GameObject.Find("background").GetComponent<SpriteRenderer>().bounds;
+        Bounds objBounds = obj.GetComponent<SpriteRenderer>().bounds;
+        if (playarea_bounds.Intersects(objBounds) == false)
+        {
+            return true;
+        }
+        return false;
     }
 }
